2622e642136ad98a8288a6516e37a7fa67dafef9
mathiasme <mathias.thibault98@gmail.com>
2022-01-01 16:40:56 +0000

    Patched atomic calculation issue with 32 bits ARM architectures
    Server was not working on my raspberry Pi. Appears the issue is related to the Go compiler (https://github.com/golang/go/issues/599). As read here https://github.com/census-instrumentation/opencensus-go/issues/587 , I switched these two lines in the counter struct and it now works.

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -10,8 +10,8 @@ import (
 )
 
 type counter struct {
-	startTime time.Time
 	count     uint64
+	startTime time.Time
 }
 
 // Counter providers a threadsafe counter to use for storing long running counts
